dd569beadf59916f0b88f26f57b5e1677b408e34
Goutham Sadubathula <goutham.sadubathula444@gmail.com>
2024-12-27 11:35:59 +0530

    initial commit - configuring new nodejs application

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    transform: {
+        "^.+\\.ts?$": "ts-jest",
+    },
+    testEnvironment: "node",
+    testPathIgnorePatterns: [],
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    roots: ["<rootDir>/src"],
+}
